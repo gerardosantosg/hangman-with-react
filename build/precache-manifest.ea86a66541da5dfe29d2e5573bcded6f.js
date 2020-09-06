7a999e188b3b87d4434e94de0ad0ad0796565e19
Gerardo Santos Gonzalez <65302754+gerardosantosg@users.noreply.github.com>
2020-09-06 13:54:39 -0500

    Add files via upload

---
+++ build/precache-manifest.ea86a66541da5dfe29d2e5573bcded6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afa38eb85866fd5b4ed739906a06477",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7111ce432ab789c5140",
+    "url": "/static/css/main.6e52d740.chunk.css"
+  },
+  {
+    "revision": "7d1d18375be8d2a6d847",
+    "url": "/static/js/2.33e03227.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33e03227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7111ce432ab789c5140",
+    "url": "/static/js/main.6e3ca688.chunk.js"
+  },
+  {
+    "revision": "d683f26c0eb0b02f5e97",
+    "url": "/static/js/runtime-main.65a41f55.js"
+  }
+]);
